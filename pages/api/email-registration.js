471d453e6ea1f67013ea6f5887f8e2ed545d8154
MaximeQuagebeur <quagebeur.maxime@gmail.com>
2023-01-08 22:21:36 +0100

    email registration in data.json with FS ok

---
+++ pages/api/email-registration.js
@@ -0,0 +1,53 @@
+import path from "path";
+import fs from "fs";
+
+function buildPath() {
+  return path.join(process.cwd(), "data", "data.json");
+}
+
+function exctractData(filePath) {
+  const jsonData = fs.readFileSync(filePath);
+  const data = JSON.parse(jsonData);
+  return data;
+}
+
+export default function handler(req, res) {
+  const { method } = req;
+  const filePath = buildPath();
+  const { events_categories, allEvents } = exctractData(filePath);
+
+  if (!allEvents) {
+    return res.json({
+      status: 404,
+      message: "Error: Events data not found",
+    });
+  }
+
+  if (method === "POST") {
+    const { email, eventId } = req.body;
+
+    const newAllEvents = allEvents.map((elem) => {
+      if (elem.id === eventId) {
+        if (elem.emails_registered.includes(email)) {
+          res
+            .status(201)
+            .json({ message: "Error: This email has already been registered" });
+        }
+        return {
+          ...elem,
+          emails_registered: [...elem.emails_registered, email],
+        };
+      }
+      return elem;
+    });
+
+    fs.writeFileSync(
+      filePath,
+      JSON.stringify({ events_categories, allEvents: newAllEvents })
+    );
+
+    res.status(200).json({
+      message: `Yous has been registered successfully with the email: ${email}`,
+    });
+  }
+}
--- src/components/Events/SingleEvent.js
+++ src/components/Events/SingleEvent.js
@@ -1,14 +1,50 @@
 import Image from "next/image";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useRef } from "react";
 
 const SingleEvent = ({ data }) => {
+  const inputEmail = useRef();
+  const router = useRouter();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const emailValue = inputEmail.current.value;
+    const eventId = router.query.id;
+
+    try {
+      const response = await fetch("/api/email-registration", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: emailValue, eventId }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+
+      const data = await response.json();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <div>
-      <Image src={data.image} width={1000} height={500} alt={data.title} />
+    <div className="event_single_page">
       <h1>{data.title}</h1>
+      <Image src={data.image} width={1000} height={500} alt={data.title} />
       <p>{data.description}</p>
-      <input type="email" placeholder="Enter your mail" />{" "}
-      <button>Submit</button>
+      <form className="email_registration" onSubmit={onSubmit}>
+        <label>Get registred for this event</label>
+        <input
+          ref={inputEmail}
+          type="email"
+          id="email"
+          placeholder="Enter your email"
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
